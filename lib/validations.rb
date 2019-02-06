917c615c35432bb635e275693315dbd5d58d2a34
Bob Lail <lail@squareup.com>
2019-02-05 22:46:45 -0600

    Read Usage from comments so extra constant isn’t required

--- lib/validations.rb
+++ lib/validations.rb
@@ -71,38 +71,38 @@ module Validations
 
 
   def puts_missing_value_message(key)
-    puts "\e[37m#{USAGE}\e[0m",
+    puts "\e[37m#{usage}\e[0m",
          "",
          "    \e[35m<#{key}>\e[0m was left blank"
   end
 
   def puts_unmatched_value_message(key, value, message)
     message = ": #{message}" if message
-    puts "\e[37m#{USAGE}\e[0m",
+    puts "\e[37m#{usage}\e[0m",
          "",
          "    \e[35m#{value.inspect}\e[0m is not a legal value for \e[35m<#{key}>\e[0m#{message}"
   end
 
   def puts_missing_file_message(key, value)
-    puts "\e[37m#{USAGE}\e[0m",
+    puts "\e[37m#{usage}\e[0m",
          "",
          "    \e[35m#{value}\e[0m is not a \e[35m<#{key}>\e[0m that exists."
   end
 
   def puts_not_a_file_message(key, value)
-    puts "\e[37m#{USAGE}\e[0m",
+    puts "\e[37m#{usage}\e[0m",
          "",
          "    \e[35m#{value}\e[0m for \e[35m<#{key}>\e[0m is not a file."
   end
 
   def puts_not_a_directory_message(key, value)
-    puts "\e[37m#{USAGE}\e[0m",
+    puts "\e[37m#{usage}\e[0m",
          "",
          "    \e[35m#{value}\e[0m for \e[35m<#{key}>\e[0m is not a directory."
   end
 
   def puts_unrecognized_value_message(key, value)
-    puts "\e[37m#{USAGE}\e[0m",
+    puts "\e[37m#{usage}\e[0m",
          "",
          "    \e[35m#{value}\e[0m is not an \e[35m<#{key}>\e[0m that I know."
   end
@@ -112,6 +112,13 @@ module Validations
     options.each do |option|
       puts "       #{option}"
     end
+  end
+
+
+
+  def usage
+    return @usage if defined?(@usage)
+    @usage = File.read($PROGRAM_NAME)[/^#\s*(Usage:.*)$/, 1]
   end
 
 end
