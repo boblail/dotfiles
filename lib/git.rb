77e0a2e5bc73bcb238311bbfa7f3fd1d912dbdcd
Bob Lail <lail@squareup.com>
2021-07-14 10:00:19 -0500

    [fix] Added support for default branches other than "master" (10m)
    Co-authored-by: Nate Pinsky <npinsky@squareup.com>

--- lib/git.rb
+++ lib/git.rb
@@ -14,6 +14,20 @@ module Git
     `git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \\(.*\\)/\\1/'`.chomp
   end
 
+  def self.default_branch
+    __default_branch ||
+        (__reset_default_branch! && __default_branch) ||
+        (raise "Couldn't figure out default branch")
+  end
+
+  def self.__default_branch
+    `git symbolic-ref -q --short refs/remotes/origin/HEAD`[/^origin\/(.+)$/, 1]
+  end
+
+  def self.__reset_default_branch!
+    echo_run! "git remote set-head origin --auto"
+  end
+
   def self.show_ref(ref_name)
     # refs/heads/ or else results include refs/remotes/...
     # and we get more than one SHA back
